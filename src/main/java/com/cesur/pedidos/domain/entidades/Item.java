f64994d8e38447d237d5ec3e82ac586b46f488bc
gabri <gabrielrl2004@gmail.com>
2023-10-26 08:13:24 +0200

    Items actualizados

--- src/main/java/com/cesur/pedidos/domain/entidades/Item.java
+++ src/main/java/com/cesur/pedidos/domain/entidades/Item.java
@@ -1,17 +1,21 @@
 package com.cesur.pedidos.domain.entidades;
 
 public class Item {
+    private int id;
     private Producto producto;
-
     private int cantidad;
+    private String codigoPedido;
+
 
     public Item() {
 
     }
 
-    public Item(Producto producto, int cantidad) {
+    public Item(Producto producto, int cantidad, int id, String codigoPedido) {
         this.producto = producto;
         this.cantidad = cantidad;
+        this.id = id;
+        this.codigoPedido = codigoPedido;
     }
 
     public Producto getProducto() {
@@ -28,5 +32,21 @@ public class Item {
 
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodigoPedido() {
+        return codigoPedido;
+    }
+
+    public void setCodigoPedido(String codigoPedido) {
+        this.codigoPedido = codigoPedido;
     }
 }
